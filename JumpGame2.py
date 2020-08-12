29695e2182984b796c113e27fbe9a201a7a2a5c3
Shantanu609 <shantanu_shinde_@mail.fresnostate.edu>
2020-08-12 14:42:51 -0700

    Updated JumpGame2.py

--- JumpGame2.py
+++ JumpGame2.py
@@ -1,8 +1,12 @@
 # Time Complexity : O(n) 
-# Space Complexity :O(n)
+# Space Complexity :O(1)
 # Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : No
 # Your code here along with comments explaining your approach
+# 1) Take first element and the that number is the maximum jumps that we can go starting from 1.
+# 2) nextInt will track the number, a number that will tell the maximum jump that we can dive, 
+#    during the iteration untill we reach the currInt that was where we started from. 
+# 3) if we reach the currInt, so we reached our inital destination, so we incremenet the jump by 1. 
 class Solution:
     def jump(self, nums):
         if len(nums) < 2:
