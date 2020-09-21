062185bc5d409770a80975221a35b7427c938b88
Arpit Sharma <arpit317@gmail.com>
2020-09-21 01:55:01 -0700

    Create JumpGameII.java

---
+++ JumpGameII.java
@@ -0,0 +1,18 @@
+//TC: O(n)
+//SC: O(1)
+class Solution {
+    public int jump(int[] nums) {
+        if(nums == null || nums.length < 2) return 0;
+        int jumps = 1;
+        int currInt = nums[0]; int nextInt = nums[0];
+        
+        for(int i = 1; i < nums.length; i++){
+            nextInt = Math.max(nextInt, i+nums[i]);
+            if(currInt != nums.length - 1 && i == currInt){
+                currInt = nextInt;
+                jumps++;
+            }
+        }
+        return jumps;
+    }
+}
