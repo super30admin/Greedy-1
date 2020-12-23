6a2ae96a48f5dc0ab1643b455d86f70803becb65
Kanika <kanika.khanna@sjsu.edu>
2020-12-23 15:19:38 -0800

    Greedy 1 done. Please review.

---
+++ Problem_3.py
@@ -0,0 +1,28 @@
+"""
+Time Complexity : O(n)- 2n to be precise
+Space Complexity : O(n)
+Did this code successfully run on Leetcode : Yes
+Any problem you faced while coding this : No
+
+Your code here along with comments explaining your approach:
+Here, we make a temp array of size of length of ratings array. We iterate through the array, if right child has 
+higher rating, we increase his candy by one from left child. After complete iteration, we traverse from
+right side and if left child has more rating, we increase his rating by one from right child. At the end,
+we calculate total number of candies.
+"""
+
+
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        l = len(ratings)
+        arr = [1 for i in range(l)]
+        total = 0
+        for i in range(1, l):
+            if ratings[i] > ratings[i-1]:
+                arr[i] = arr[i-1]+1
+        total += arr[-1]
+        for i in range(l-2, -1, -1):
+            if ratings[i] > ratings[i+1] and not arr[i] > arr[i+1]:
+                arr[i] = arr[i+1]+1
+            total += arr[i]
+        return total
