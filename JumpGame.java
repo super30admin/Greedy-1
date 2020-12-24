5facc5108e7227cdc0ae891f1056cc7c97b472ae
pkariya1 <pkariya1@binghamton.edu>
2020-12-24 14:21:38 +0530

    Jump game 1 completed

--- JumpGame.java
+++ JumpGame.java
@@ -1,5 +1,15 @@
 public class JumpGame {
 	public boolean canJump(int[] nums) {
+		if (nums == null || nums.length == 0)
+			return false;
 
+		int destination = nums.length - 1;
+		int n = nums.length - 1;
+		for (int i = n; i >= 0; i--) {
+			if (i + nums[i] >= destination) {
+				destination = i;
+			}
+		}
+		return destination == 0;
 	}
 }
