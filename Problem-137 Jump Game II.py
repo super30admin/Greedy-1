6f275e503616441f8b99d1f539078decb2271403
Adhish Shrivastava <adhish70@gmail.com>
2021-12-30 11:03:46 -0500

    Problem 137 done

--- Problem-137 Jump Game II.py
+++ Problem-137 Jump Game II.py
@@ -1,11 +1,24 @@
 # 45. Jump Game II
 # https://leetcode.com/problems/jump-game-ii/
 
-# Logic: 
+# Logic: We create a DP array. We iterate over the input, and for 
+# the value of the item from input, we set the next values as the min steps to reach it.
+# After the iteration is complete, ther last index in the dp array 
+# will be our answer. The defination of each cell is, min steps 
+# from the start to reach this cell.
 
-# Time Complexity: 
-# Space Complexity: 
+# Time Complexity: O(n^2)
+# Space Complexity: O(n)
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        pass
+        n = len(nums)
+        dp = [inf for i in range(n)]
+        dp[0] = 0
+        
+        for i in range(n):
+            for j in range(1, nums[i] + 1):
+                if i + j < n:
+                    dp[i+j] = min(dp[i+j], dp[i]+1)
+        
+        return dp[n-1]
