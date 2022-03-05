d26faf43d64f539220ce0e932826ed6c3011c262
BitLegion1996 <93818475+BitLegion1996@users.noreply.github.com>
2022-03-04 17:11:23 -0700

    Update canJump.py

--- canJump.py
+++ canJump.py
@@ -1,5 +1,42 @@
 class Solution:
-    def BruteForcecanJump(self, nums: List[int]) -> bool:
+    def __init__(self):
+        self.dp = [] 
+        
+    def TopDowndfs(self, nums, idx):
+        if idx >= len(nums)-1: return True
+        if  self.dp[idx] != -1:
+            return self.dp[idx] == 1 
+        for j in range(1, nums[idx]+1):
+            result = self.TopDowndfs(nums, idx+j)
+            if result:
+                self.dp[idx] = 1
+                return True
+        self.dp[idx] = 0
+        return False
+	
+    def canJump(self, nums: List[int]) -> bool:
+        self.dp = [-1]*( len(nums)-1 )
+        return self.TopDowndfs(nums, 0)
+		
+        
+    def BruteForceBFScanJump(self, nums: List[int]) -> bool:
+        visited = set([])
+        visited.add(0)
+        q = [] 
+        q.append(0)
+        while q:
+            curr = q.pop(0)
+            if curr == len(nums)-1:
+                return True
+            for i in range(1, nums[curr]+1):
+                if curr + i == len(nums)-1:
+                    return True
+                if curr+i  not in visited:
+                    visited.add( curr+i )
+                    q.append( curr+i )
+        return False
+                    
+    def BruteForceDFScanJump(self, nums: List[int]) -> bool:
         '''
         Algorithm :
         1. Basic idea is to try all possible jumps for all the elements. 
@@ -18,4 +55,7 @@ class Solution:
             return False
         
         return dfs(nums, 0)
+    
+    
+    
     
