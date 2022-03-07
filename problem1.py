89d6e6b34e55e4b4e4a95915a5eee6e9a5ed13f6
nikhil <mandgenikhil@gmail.com>
2022-03-07 14:30:41 -0500

    Create problem1.py

---
+++ problem1.py
@@ -0,0 +1,17 @@
+#TC: O(n)
+#SC: O(n)
+
+class Solution:
+    def candy(self, r: List[int]) -> int:
+        rlen = len(r)
+        can = [1 for _ in r]
+
+        for i in range(1, rlen):
+            if r[i] > r[i - 1]:
+                can[i] = can[i - 1] + 1
+
+        for i in range(rlen - 2, -1, -1):
+            if r[i] > r[i + 1]:
+                can[i] = max(can[i], can[i + 1] + 1)
+
+        return sum(can)
