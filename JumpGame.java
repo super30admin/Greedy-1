d8d0be0075ac3dd29fdfac915861c8b5612152d7
harshaanirudh <harsha.anirudh@gmail.com>
2022-03-18 18:55:07 -0700

    Create JumpGame.java

---
+++ JumpGame.java
@@ -0,0 +1,14 @@
+//Greedy Approach
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+class Solution {
+    public boolean canJump(int[] nums) {
+        int destination=nums.length-1;
+        for(int i=nums.length-2;i>=0;i--){
+            if(i+nums[i]>=destination){
+                destination=i;
+            }
+        }
+        return destination==0;
+    }
+}
