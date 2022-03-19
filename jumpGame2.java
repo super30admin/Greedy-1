9225f6565fb3eabcb1037c576c3231ccf94018c2
harshaanirudh <harsha.anirudh@gmail.com>
2022-03-18 18:54:39 -0700

    Create jumpGame2.java

---
+++ jumpGame2.java
@@ -0,0 +1,15 @@
+//Time Complexity: O(n)
+//SpaceComplexity: O(1)
+class Solution {
+    public int jump(int[] A) {
+	int jumps = 0, curEnd = 0, curFarthest = 0;
+	for (int i = 0; i < A.length - 1; i++) {
+		curFarthest = Math.max(curFarthest, i + A[i]);
+		if (i == curEnd) {
+			jumps++;
+			curEnd = curFarthest;
+		}
+	}
+	return jumps;
+}
+}
