988d2c48c216294dcf18390090b5b8dd13d9928e
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-08-09 12:24:15 -0400

    Update sample.java

--- sample.java
+++ sample.java
@@ -25,3 +25,43 @@ class Solution {
 }
 
 // Your code here along with comments explaining your approach
+
+
+//*****CANDY****\
+
+//Greedy 
+//Take one decision at a time
+
+class Solution {
+    public int candy(int[] ratings) {
+        
+        
+        if(ratings.length==0 || ratings==null) return 0;
+        int n=ratings.length;
+        
+        if(n==1) return 1;
+        int[] ans=new int[n];
+        if(ratings.length==1) return 1;
+        Arrays.fill(ans, 1);
+        //forward pass
+        for(int i=1;i<n;i++)
+        {
+            if(ratings[i]>ratings[i-1])
+            {
+                ans[i]=1+ ans[i-1];
+            }
+        }
+        
+        //Backward pass
+        int sum=ans[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            if(ratings[i]>ratings[i+1])
+            {
+                ans[i]=Math.max(ans[i], 1+ ans[i+1]);
+            }
+            sum += ans[i];
+        }
+        return sum;
+    }
+}
