abaea0aa073cd8472d4e9149479a1ffdf19d78fa
Shubham <shub01gupta@gmail.com>
2022-09-04 20:58:11 +0530

    Completed Greedy-1

---
+++ jumpgame.java
@@ -0,0 +1,19 @@
+// TC: O(n)
+// SC: O(1)
+
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        if(nums.length < 2) return true;
+
+        int n = nums.length;
+        int destination = n - 1;
+
+        for(int i = n - 2; i >= 0; i--){
+            if(i + nums[i] >= destination){
+                destination = i;
+            }
+        }
+        return destination == 0;
+    }
+}
