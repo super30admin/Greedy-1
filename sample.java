d5860c5bb221fc2b04d1708765439c62ab032cb6
MalavyaRaval <malavyaraval@gmail.com>
2023-03-23 14:38:50 -0700

    Completed

--- sample.java
+++ sample.java
@@ -1,7 +1,20 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(n)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :No
 
 
 // Your code here along with comments explaining your approach
+class Solution {
+    public boolean canJump(int[] nums) {
+        if(nums.length == 1) return true;
+        int n = nums.length;
+        int target = n-1;
+        for(int i = n-2;i>=0;i--){
+            if(i+nums[i]>=target){
+                target=i;
+            }
+        }
+        return target==0;
+    }
+}
