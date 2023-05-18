55e3cb7047e8eee4eb51c867b4bb05e0f082fb85
Bipin <48069462+bipin44@users.noreply.github.com>
2023-05-18 11:04:31 -0500

    Please review Problem 1 ,2 & 3

---
+++ Problem3.java
@@ -0,0 +1,27 @@
+// Time: O(n) ratings length
+// space : O(n) 
+
+class Solution {
+    public int candy(int[] ratings) {
+        int[] res = new int[ratings.length];
+        Arrays.fill(res,1);
+
+        //forward pass
+        for(int i = 1; i<ratings.length ; i++){
+            if(ratings[i]> ratings[i-1]){
+                res[i] = res[i-1] +1;
+            }
+        }
+
+        // backward 
+        int sum = res[ratings.length-1];
+        for(int i = ratings.length-2 ; i >=0 ; i--){
+            if(ratings[i] > ratings[i+1]){
+                res[i] = Math.max(res[i], res[i+1]+1);
+            }
+            sum = sum + res[i];
+        }
+        System.out.println(Arrays.toString(res));
+        return sum;
+    }
+}
