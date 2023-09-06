56f43361b7e8ec5016d2650a13ac01c5210046bb
Pratyush Srivastava <pratyshsrivastava96@gmail.com>
2023-09-06 12:29:52 -0700

    Candy Problem

--- sample.java
+++ sample.java
@@ -5,3 +5,32 @@
 
 
 // Your code here along with comments explaining your approach
+//Candy problem
+class Solution {
+    public int candy(int[] ratings) {
+
+        int sum = 0;
+        int n = ratings.length;
+        int[] result = new int[n];
+        Arrays.fill(result,1); //Giving each kid one candy
+        //Left pass
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                result[i] = result[i-1] +1;
+            }
+        }
+
+        //now right pass
+        sum = result[n-1];
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                result[i] = Math.max(result[i],result[i+1]+1);
+            }
+            sum += result[i];
+        }
+
+        return sum;
+
+        
+    }
+}
