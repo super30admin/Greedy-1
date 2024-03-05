46ce6e8c6d5f3354278214be00ebf457f8ed4fa4
pallavithorat <thoratpallavi2000@gmail.com>
2024-03-04 16:29:50 -0800

    Greedy-1 Completed

---
+++ Problem3.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    public int candy(int[] ratings) {
+        //TC : O(N)
+        if(ratings == null || ratings.length == 0) return 0;
+        int n = ratings.length;
+
+        int [] result = new int[n];
+
+        Arrays.fill(result,1);
+        
+
+        //left pass
+        for(int i = 1 ; i<n; i++){
+            if(ratings[i] > ratings[i-1]){
+                result[i] = result[i-1] + 1;
+            }
+        }
+
+        int sum = result[n-1];
+
+        //right pass
+        for(int i = n-2; i >= 0 ; i--){
+            if(ratings[i] > ratings[i+1]){
+                result[i] = Math.max(result[i] , result[i+1]+1);
+            }
+            sum += result[i];
+        }
+        return sum;
+    }
+}
